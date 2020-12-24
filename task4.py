571a20839214c2757c37108a3f30c926b1a88430
E-Sakhno <miss.saxno@yandex.ru>
2020-12-25 02:48:46 +0300

    Задание 4 по п. 12

---
+++ task4.py
@@ -0,0 +1,28 @@
+def getInput():
+    inp = input("Введите символы: ")
+    return inp
+
+
+def testInput(value):
+    try:
+        int(value)
+        return True
+    except:
+        return False
+
+
+def strToInt(value):
+    return int(value)
+
+
+def printInt(value):
+    print(value)
+
+
+if __name__ == '__main__':
+    a = getInput()
+    if testInput(a):
+        a_int = strToInt(a)
+        printInt(a_int)
+
+
